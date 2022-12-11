9716d3ef10c552e9dc86312d95e6464ee8dc7648
czapkazdaszkiem <jaroslaw.gawrych@gmail.com>
2022-12-11 22:20:22 +0100

    dzięki
    dzięki działa

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Działa?");
         System.out.println("Chyba tak?");
+        System.out.println("działa w chuj");
     }
 }
